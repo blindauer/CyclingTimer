512ef5e79a261118df2f5a53461ae0dab05232fe
Maria Civilis <maria.civilis@gmail.com>
2021-01-28 10:27:43 -0500

    Added TimerProgressView

---
+++ CyclingTimer/TimerDetail.swift
@@ -0,0 +1,29 @@
+//
+//  TimerDetail.swift
+//  CyclingTimer
+//
+//  Created by Maria Civilis on 2021-01-27.
+//
+
+import SwiftUI
+
+struct TimerDetail: View {
+    var timer: Timer
+    @State var progressValue = Float.zero
+    
+    var body: some View {
+        ZStack {
+            TimerProgressView(progress: $progressValue, set: timer.segments[0].sets[0])
+                .frame(width: 150.0, height: 150.0)
+                .padding(40.0)
+            Spacer()
+        }
+
+    }
+}
+
+struct TimerDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerDetail(timer: testData[0])
+    }
+}
---
+++ CyclingTimer/TimerProgressView.swift
@@ -0,0 +1,38 @@
+//
+//  TimerProgressView.swift
+//  CyclingTimer
+//
+//  Created by Maria Civilis on 2021-01-27.
+//
+
+import SwiftUI
+
+struct TimerProgressView: View {
+    @Binding var progress: Float
+    var set: Set
+    
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 20.0)
+                .opacity(0.3)
+                .foregroundColor(Color.blue)
+            
+            Circle()
+                .trim(from: 0.0, to: CGFloat(progress))
+                .stroke(style: StrokeStyle(lineWidth: 20.0, lineCap: .round, lineJoin: .round))
+                .foregroundColor(Color.blue)
+                .rotationEffect(Angle(degrees: 270.0))
+                .animation(.linear)
+            
+            VStack {
+                Text("1:07")
+                Rectangle()
+                    .frame(height: 2.0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Text("\(set.rpm) RPM")
+            }
+            .padding(40.0)
+        }
+    }
+}
