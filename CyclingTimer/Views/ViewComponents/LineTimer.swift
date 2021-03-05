067c9ac15a13acc7c46d75a93fbbeba83f5660d5
Maria Civilis <maria.civilis@gmail.com>
2021-03-04 22:11:16 -0500

    Fix line timer

--- CyclingTimer/Views/ViewComponents/LineTimer.swift
+++ CyclingTimer/Views/ViewComponents/LineTimer.swift
@@ -10,28 +10,73 @@ import SwiftUI
 struct LineTimer: View {
     @ObservedObject var timerManager: TimerManager
     
-    var backgroundCircle: some View {
+    let sessionLineWidth: CGFloat = 10.0
+    let setLineWidth: CGFloat = 30.0
+    let setPadding: CGFloat = 80.0
+    let sessionPadding: CGFloat = 20.0
+    
+    var animatedCircles: some View {
         Circle()
-            .stroke(lineWidth: 40.0)
-            .opacity(0.3)
-            .foregroundColor(.accentColor)
+            .stroke(style: StrokeStyle(lineWidth: 2.0, lineCap: .round, lineJoin: .round))
+            .foregroundColor(timerManager.setColor)
+            .padding(timerManager.timerState == .running ? startPadding() : endPadding())
+            .animation(Animation.linear(duration: 1).repeatForever(autoreverses: true))
     }
     
-    var progressCircle: some View {
+
+    
+    var setBackgroundCircle: some View {
+        Circle()
+            .stroke(lineWidth: setLineWidth)
+            .opacity(0.3)
+            .foregroundColor(timerManager.setColor)
+            .padding(setPadding)
+    }
+    
+    var setProgressCircle: some View {
         Circle()
             .trim(from: 0.0, to: CGFloat(timerManager.setProgress))
-            .stroke(style: StrokeStyle(lineWidth: 40.0, lineCap: .round, lineJoin: .round))
-            .foregroundColor(.accentColor)
+            .stroke(style: StrokeStyle(lineWidth: setLineWidth, lineCap: .round, lineJoin: .round))
+            .foregroundColor(timerManager.setColor)
             .rotationEffect(Angle(degrees: 270.0))
             .animation(timerManager.animation ? .linear : .none)
+            .padding(setPadding)
+    }
+    
+    var sessionBackgroundCircle: some View {
+        Circle()
+            .stroke(lineWidth: sessionLineWidth)
+            .opacity(0.3)
+            .foregroundColor(timerManager.setColor)
+            .padding(sessionPadding)
+    }
+    
+    var sessionProgressCircle: some View {
+        Circle()
+            .trim(from: 0.0, to: CGFloat(timerManager.sessionProgress))
+            .stroke(style: StrokeStyle(lineWidth: sessionLineWidth, lineCap: .round, lineJoin: .round))
+            .foregroundColor(timerManager.setColor)
+            .rotationEffect(Angle(degrees: 270.0))
+            .animation(timerManager.animation ? .linear : .none)
+            .padding(sessionPadding)
     }
     
     var body: some View {
         ZStack {
-            backgroundCircle
-            progressCircle
+            animatedCircles
+            sessionBackgroundCircle
+            sessionProgressCircle
+            setBackgroundCircle
+            setProgressCircle
         }
-        .padding(40.0)
+    }
+    
+    private func startPadding() -> CGFloat {
+        return sessionPadding - sessionLineWidth / 2 + sessionLineWidth
+    }
+    
+    private func endPadding() -> CGFloat {
+        return setPadding - setLineWidth / 2
     }
 }
 
