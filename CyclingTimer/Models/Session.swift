861fcf0b41e7455f57c98c79e5723d3b30f27849
Maria Civilis <maria.civilis@gmail.com>
2021-03-09 21:21:52 -0500

    some changes to initial data

--- CyclingTimer/Models/Session.swift
+++ CyclingTimer/Models/Session.swift
@@ -61,53 +61,21 @@ extension Session {
 
 extension Session {
     static var data: [Session] {
-        [Session(name: "Morning Workout", segments: [
+        [Session(name: "Sample Session 1", segments: [
             Segment(sets: [Set(description: "Warmup", duration: 120, rpm: 75, effort: 4)], type: .Warmup),
             
             Segment(sets: [Set(description: "Torque", duration: 60, rpm: 60, effort: 9),
                            Set(description: "Rev", duration: 60, rpm: 110, effort: 10),
-                           Set(description: "Recovery", duration: 120, rpm: 85, effort: 3)], repetitions: 5, type: .Work)
-        ]),
-        Session(name: "HIIT Workout", segments: [
-            Segment(sets: [Set(description: "Warmup", duration: 180, rpm: 70, effort: 4),
-                           Set(description: "Warmup", duration: 120, rpm: 80, effort: 7),
-                           Set(description: "Warmup", duration: 60, rpm: 90, effort: 8),
-                           Set(description: "Recovery", duration: 120, rpm: 75, effort: 4)], type: .Warmup),
-            
-            Segment(sets: [Set(description: "All out", duration: 30, rpm: 100, effort: 10),
-                           Set(description: "Recovery", duration: 60, rpm: 85, effort: 3)], repetitions: 16, type: .Work),
-            
-            Segment(sets: [Set(description: "Cooldown", duration: 180, rpm: 75, effort: 4)], type: .Cooldown)
-        ]),
-        Session(name: "Forty twenties", segments: [
-            Segment(sets: [Set(description: "Warmup", duration: 60, rpm: 100, effort: 7),
-                           Set(description: "Warmup", duration: 60, rpm: 90, effort: 4)], type: .Warmup),
-            
-            Segment(sets: [Set(description: "40", duration: 40, rpm: 90, effort: 9),
-                           Set(description: "20", duration: 20, rpm: 75, effort: 3)], repetitions: 5, type: .Work),
-            
-            Segment(sets: [Set(description: "Recovery", duration: 150, rpm: 75, effort: 4)], type: .Work),
-            
-            Segment(sets: [Set(description: "40", duration: 40, rpm: 90, effort: 9),
-                           Set(description: "20", duration: 20, rpm: 75, effort: 3)], repetitions: 5, type: .Work),
-            
-            Segment(sets: [Set(description: "Recovery", duration: 150, rpm: 75, effort: 4)], type: .Work),
-            
-            Segment(sets: [Set(description: "40", duration: 40, rpm: 90, effort: 9),
-                           Set(description: "20", duration: 20, rpm: 75, effort: 3)], repetitions: 5, type: .Work),
-            
-            Segment(sets: [Set(description: "Recovery", duration: 150, rpm: 75, effort: 4)], type: .Work),
-            
-            Segment(sets: [Set(description: "40", duration: 40, rpm: 90, effort: 9),
-                           Set(description: "20", duration: 20, rpm: 75, effort: 3)], repetitions: 5, type: .Work),
-            
-            Segment(sets: [Set(description: "Recovery", duration: 150, rpm: 75, effort: 4)], type: .Work),
-            
-            Segment(sets: [Set(description: "40", duration: 40, rpm: 90, effort: 9),
-                           Set(description: "20", duration: 20, rpm: 75, effort: 3)], repetitions: 5, type: .Work),
-            
-            Segment(sets: [Set(description: "Cooldown", duration: 180, rpm: 75, effort: 4)], type: .Cooldown)
-        ])
-        ]
+                           Set(description: "Recovery", duration: 120, rpm: 85, effort: 3)], repetitions: 5, type: .Work)]),
+         Session(name: "Sample Session 2", segments: [
+             Segment(sets: [Set(description: "Warmup", duration: 120, rpm: 75, effort: 4)], type: .Warmup),
+             
+             Segment(sets: [Set(description: "Max Effort", duration: 30, rpm: 60, effort: 9),
+                            Set(description: "Recovery", duration: 60, rpm: 110, effort: 10)], repetitions: 10, type: .Work),
+             
+             Segment(sets: [Set(description: "Cooldown", duration: 120, rpm: 75, effort: 4)], type: .Cooldown)]),
+         Session(name: "Sample Session 3", segments: [
+             Segment(sets: [Set(description: "Work", duration: 120, rpm: 95, effort: 8),
+                            Set(description: "Recovery", duration: 120, rpm: 80, effort: 6)], repetitions: 10, type: .Work)])]
     }
 }
